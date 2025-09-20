3e0e64388894a6273ff4db55b2cce00a1a1128a0
Eunkyung-Chung <dmsrud0726@gmail.com>
2025-09-20 15:07:59 +0900

    forth commit

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ const searchButton = document.querySelector(".input-area button");
 searchButton.addEventListener("click", (event) => 
 getNewsByKeyword(event));
 
+//head-line 클릭이벤트
+document.querySelector(".head-line").addEventListener("click", () => {
+    page = 1;
+    getLatestNews();
+})
+
 //검색창에 enter key 눌렀을때 키워드별 뉴스 검색
 function enterKey() {
     if (window.event.keyCode == 13){
@@ -30,12 +36,19 @@ function enterKey() {
 }
 
 //code refactoring --> url을 전역변수로
-let url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`);
+let url = new URL (` https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`);
+
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 //code refactoring --> 중복되는 코드 따로 모아서 함수로
 const getNews = async() => {
     let data = null;
     try {
+        url.searchParams.set("page", page) //&page=page
+        url.searchParams.set("pageSize", pageSize); //&pageSize=pageSize 
         const response = await fetch(url);
         console.log("rrr",response);
         data = await response.json();
@@ -54,13 +67,15 @@ const getNews = async() => {
     }
      console.log("data", data);
      newsList = data.articles;
+     totalResults = data.totalResults;
      console.log("newsList", newsList);
      render();
+     paginationRender();
 }
 
 const getLatestNews = async () => {
     //url instance만들기
-    url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`);
+    url = new URL (` https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`);
     getNews();
 };
 
@@ -69,7 +84,8 @@ const getLatestNews = async () => {
 const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase(); 
     console.log("category", category);
-    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}`)
+    url = new URL(` https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}`);
+    page = 1;
     getNews();  
 };
 
@@ -78,7 +94,7 @@ const getNewsByKeyword = async(event) => {
     // console.log("goclick");
     const keyword = document.querySelector(".search-input").value;
     // console.log("keyword",keyword);
-    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}`);
+    url = new URL(` https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}`);
     getNews();
 }
 
@@ -102,7 +118,7 @@ const render = (error) => {
                   />
                </div>
                <div class="col-lg-8">
-                  <h2>${news.title}</h2>
+                  <h2><a href="${news.url}">${news.title}</a></h2>
                   <p class="context">
                     ${news.description ? news.description : "내용없음"}
                   </p>
@@ -115,6 +131,73 @@ const render = (error) => {
 
     document.getElementById('news-board').innerHTML = newsHTML;
 };
+
+const paginationRender = () => {
+    //totalResults :받아오는 총 데이터 개수, api에서 주어짐
+    //page : 현재 보고 있는 페이지, api에서 주어짐
+    //pageSize :한 페이지에 몇개씩 데이터 받을 것인가,내가 정함 
+    //groupSize : 몇개의 페이지를 한 그룹으로 묶을까, 내가 정함
+
+    //totalPage: 몇개의 페이지가 필요한가, Math.ceil(totalResult / pageSize)
+    const totalPages = Math.ceil(totalResults / pageSize);
+    //pageGroup: 몇번째 페이지 그룹인가(그룹 번호), Math.ceil(page / groupSize)
+    const pageGroup = Math.ceil(page / groupSize);
+    //lastPage : groupSize * pageGroup
+    let lastPage = pageGroup * groupSize;
+    if(lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+
+    // firstPage : lastPage - (groupSize - 1)
+    const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1); 
+  
+    
+  
+    let paginationHTML = `<li class="page-item first-btn" onclick="moveToPage(1)">
+      <a class="page-link" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>`;
+
+    paginationHTML += ` <li class="page-item previous-btn" onclick="moveToPage(${page-1})"><a class="page-link">Previous</a></li>`;
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML +=  `<li class="page-item ${i===page?"active":''}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    }
+
+    paginationHTML += `<li class="page-item next-btn" onclick="moveToPage(${page+1})"><a class="page-link">Next</a></li>`;
+
+    paginationHTML += `   <li class="page-item last-btn" onclick="moveToPage(${totalPages})">
+      <a class="page-link" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>`;
+
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+
+    //첫번째와 마지막 페이지에서 move-button 안보이기
+    if(page === 1) {
+       let movePageLi = document.querySelectorAll(".first-btn, .previous-btn");
+       console.log(movePageLi);
+        movePageLi.forEach((li) => {
+            li.style.display = "none";
+        });
+    } 
+    if (page === totalPages) {
+         let movePageLi = document.querySelectorAll(".last-btn, .next-btn");
+       console.log(movePageLi);
+        movePageLi.forEach((li) => {
+            li.style.display = "none";
+        });
+    }
+}
+
+//페이지 클릭했을때 그 페이지에 해당하는 뉴스 render
+const moveToPage = (pageNum) => {
+    console.log("movetopage", pageNum);
+    page = pageNum;
+    getNews();
+}
 
 getLatestNews();
 
